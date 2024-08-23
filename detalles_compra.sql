bc3ed34b8ed32bffcf38fccad715a2acefe6580b
steffany cobos <cobosleandra2@gmail.com>
2024-08-23 12:10:55 -0400

    2DA PREENTREGA

---
+++ detalles_compra.sql
@@ -0,0 +1,13 @@
+insert into detalles_compra (id_compra , id_producto , cantidad) values (4, 1, 4);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (3, 5, 3);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (4, 9, 4);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (6, 6, 5);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (3, 4, 2);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (7, 15, 5);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (5, 29, 4);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (2, 8, 5);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (15, 3, 5);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (12, 4, 2);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (15, 27, 4);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (2, 24, 5);
+insert into detalles_compra (id_compra , id_producto , cantidad) values (11, 66, 2);
